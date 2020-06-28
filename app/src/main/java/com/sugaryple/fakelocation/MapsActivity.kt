890750a9868939561ce9cf41ba5566dc91cf32ba
semyeong.son <seaud5@gmail.com>
2020-06-28 12:07:05 +0900

    アプリを起きた時に自分の位置に移動

--- app/src/main/java/com/sugaryple/fakelocation/MapsActivity.kt
+++ app/src/main/java/com/sugaryple/fakelocation/MapsActivity.kt
@@ -1,20 +1,22 @@
 package com.sugaryple.fakelocation
 
 import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import com.eazypermissions.common.model.PermissionResult
 import com.eazypermissions.livedatapermission.PermissionManager
-
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.libraries.maps.CameraUpdateFactory
 import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.OnMapReadyCallback
 import com.google.android.libraries.maps.SupportMapFragment
 import com.google.android.libraries.maps.model.LatLng
-import com.google.android.libraries.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, PermissionManager.PermissionObserver {
 
@@ -22,16 +24,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, PermissionManager.
         const val PERMISSION_REQUEST_ID = 100
     }
 
-    private lateinit var mMap: GoogleMap
+    private var map: GoogleMap? = null
+    private var fusedLocationClient: FusedLocationProviderClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        enableMyLocation()
+        moveMyLocation()
+    }
+
+    private fun moveMyLocation() {
+        if (checkSelfPositionPermission()) {
+            fusedLocationClient?.lastLocation?.addOnSuccessListener {
+                map?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude, it.longitude), 15f))
+            }
+        }
+    }
+
+    private fun requestPermission() {
         PermissionManager.requestPermissions(
             this,
             PERMISSION_REQUEST_ID,
@@ -40,23 +61,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, PermissionManager.
         )
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun enableMyLocation() {
+        if (checkSelfPositionPermission()) {
+            map?.isMyLocationEnabled = true
+        } else {
+            requestPermission()
+        }
     }
+
+    // 위치를
+    private fun checkSelfPositionPermission(): Boolean {
+        return !(ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED)
+    }
+
 
     override fun setupObserver(permissionResultLiveData: LiveData<PermissionResult>) {
         permissionResultLiveData.observe(this, Observer<PermissionResult> {
@@ -64,6 +87,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, PermissionManager.
                 is PermissionResult.PermissionGranted -> {
                     if (it.requestCode == PERMISSION_REQUEST_ID) {
                         // 사용자가 권한을 부여한 후 여기에 논리를 추가하십시오.
+                        enableMyLocation()
                         Toast.makeText(this@MapsActivity, "PermissionGranted", Toast.LENGTH_SHORT).show()
                     }
                 }
