4ee84c3b7f776323c6a365a59c3544463e33e218
semyeong.son <seaud5@gmail.com>
2020-08-11 00:29:25 +0900

    koinバグ修正

--- app/src/main/java/com/sugaryple/fakelocation/MyKoin.kt
+++ app/src/main/java/com/sugaryple/fakelocation/MyKoin.kt
@@ -6,20 +6,25 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
+import org.koin.core.logger.EmptyLogger
 import org.koin.dsl.module
 
 class MyKoin {
     fun start(applicationContext: Context) {
         startKoin {
-            androidLogger()
+            if (BuildConfig.DEBUG) {
+                androidLogger()
+            } else {
+                EmptyLogger()
+            }
             androidContext(applicationContext)
             modules(getModules())
         }
     }
 
-    private fun getModules() = module {
+    private fun getModules() = listOf(
         getViewModels()
-    }
+    )
 
     private fun getViewModels() = module {
         viewModel { MapsViewModel() }
